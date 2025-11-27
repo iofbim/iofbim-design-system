04def0d65f8454a4b939d978fbca161dfd9f29c8
ruornil <ctunaboylu@gmail.com>
2025-11-27 18:38:11 +0300

    card with no background added to the design system

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,3 +21,21 @@ export const Card = ({
         </div>
     );
 };
+
+export const CardNoBG = ({
+    className,
+    children,
+    ...other
+}: CardProps) => {
+    return (
+        <div
+            className={twMerge(
+                "rounded-3xl relative z-overflow-hidden overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none",
+                className
+            )}
+            {...other}
+        >
+            {children}
+        </div>
+    );
+};
